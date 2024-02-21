88c142fafe090832af1a574c3ed3a81f3c032def
kimhappy <kimsvrkimsvr@gmail.com>
2024-02-21 21:50:07 +0900

    ref: update example

--- bindings/go/test.go
+++ bindings/go/test.go
@@ -12,13 +12,20 @@ import (
 )
 
 func main() {
-	var from [ 100 ]float32
-	var to   [   2 ]float32
+	// 클라이언트한테 받은 데이터를 저장할 배열
+	var from[ 100 ]float32
 
-	from[ 0 ] = 100
-	from[ 1 ] = 200
+	// 추론 결과가 저장될 배열
+	var to[ 2 ]float32
 
-	C.inference((*C.float)(unsafe.Pointer(&from[ 0 ])), (*C.float)(unsafe.Pointer(&to[ 0 ])))
+	// 클라이언트한테 데이터 받아서 from에 저장
+	// ...
 
+	// 추론 결과를 to 배열에 저장
+	C.inference(
+		(*C.float)(unsafe.Pointer(&from[ 0 ])),
+		(*C.float)(unsafe.Pointer(&to[ 0 ])))
+
+	// 추론 결과 출력
 	fmt.Println(to[ 0 ], to[ 1 ])
 }
